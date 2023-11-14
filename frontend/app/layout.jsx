a7ecfcffbddb61eeabba331fdc07e99519748ae3
Nicolas Coquelet <nicolascoquelet@gmail.com>
2023-11-14 19:32:35 +0100

    add rainbowkit button

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,12 +1,36 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+"use client";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { hardhat } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
-const inter = Inter({ subsets: ["latin"] });
+const { chains, publicClient } = configureChains([hardhat], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: "55d46459705f7d000d66cff31bf96971",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: false,
+  connectors,
+  publicClient,
+});
+
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains}>
+            <ChakraProvider>{children}</ChakraProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </body>
     </html>
   );
 }
