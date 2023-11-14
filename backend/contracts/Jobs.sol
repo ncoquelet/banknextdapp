d7ddbe04b9afa05b2619058ac50ce4739cf47117
salma-khan <salmakhanchouch@MacBook-Air-de-Salma.local>
2023-11-14 19:20:34 +0100

    implement methods

--- backend/contracts/Jobs.sol
+++ backend/contracts/Jobs.sol
@@ -27,9 +27,33 @@ contract Jobs {
         uint pricePaid
     );
 
-    function addJob(string calldata _description) external payable {}
+    error NotAuthorized(address);
+    error AlreadyTaken();
 
-    function takeJob(uint _id) external {}
+    function addJob(string calldata _description) external payable {
+        Job memory job = Job(msg.sender,address(0) ,_description, msg.value, false) ;
+        jobs.push(job);
+        emit jobAdded(msg.sender,_description,msg.value,jobs.length-1,job.isFinished);
+        
+    }
 
-    function setIsFinishedAndPay(uint _id) external {}
+    function takeJob(uint _id) external {
+        if(jobs[_id].worker!=address(0)){
+            revert AlreadyTaken();
+        }
+
+        jobs[_id].worker = msg.sender;
+        emit jobTaken(msg.sender, _id);
+    }
+
+    function setIsFinishedAndPay(uint _id) external {
+        if(msg.sender != jobs[_id].author){
+           revert NotAuthorized(msg.sender); 
+        }
+       
+        jobs[_id].isFinished = true;
+        (bool result,)= jobs[_id].worker.call{value: jobs[_id].price}("");
+        require(result, "Not paid");
+        emit jobIsFinishedAndPaid(jobs[_id].author, jobs[_id].worker, _id, jobs[_id].price);
+    }
 }
